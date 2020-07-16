09a27a52d2a73c2ecc8118790c81126e272d2cc2
Sameet Sapra <ssapra@fb.com>
2020-07-15 20:15:33 -0700

    Clean up comments

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ driver.get('https://accounts.shopify.com/store-login')
 login = driver.find_element_by_name('shop[domain]')
 login.send_keys(shopify_url)
 driver.find_element_by_name('commit').click()
-# driver.implicitly_wait(3) # seconds
 
 email = driver.find_element_by_name('account[email]')
 email.send_keys(email)
@@ -56,28 +55,25 @@ driver.get(oberlo_url)
 
 driver.get('https://app.oberlo.com/my-products')
 
-# get current page
-
-## for loop - while currentPage < maxPages && have not reached last product
-
-all_listings = driver.find_element_by_class_name('resource-list')
-
 # Clicking one product lets us see all 20 products.
-
+# How? The page has some JavaScript that contains all the data we need.
+all_listings = driver.find_element_by_class_name('resource-list')
 product = all_listings.find_elements_by_tag_name('a')[0]
 product_page_url = product.get_attribute('href')
 
-# open tab
+# Open tab
 driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
 
 # Load a page
 driver.get(product_page_url)
 
+# Close the tab
 driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 'w')
 all_page_text = driver.page_source
+# Find the json on the page that starts with myProducts
 json_object_start = all_page_text[all_page_text.find("myProducts: [{")+len("myProducts: "):]
 json_object = json_object_start[:json_object_start.find("filters")].strip()
 json_object_no_comma = json_object[:len(json_object)-1]
 print(json.loads(json_object_no_comma))
 
-# Now click navigate and go to next page and repeat
+# Click navigate, go to next page, and repeat
